a11a29034e03198ab785f8d0fd868e088a603ad0
sara_ali <sara.ali.mohammed213@gmail.com>
2017-12-04 11:28:42 +0200

    shuffle the list of cards

--- js/app.js
+++ js/app.js
@@ -13,6 +13,17 @@ $(document).ready(function () {
             if($('.fa-star-o').length !== 0)
                 $('.stars i').removeClass('fa-star-o').addClass('fa-star');
             $('.deck').html("");
+            app.shuffle();
+        },
+        shuffle: function () {
+            let random = 0;
+            let temp = 0;
+            for (let i = 1 ; i < app.cards.length ; i++) {
+                random = Math.round(Math.random() * i);
+                temp = app.cards[i];
+                app.cards[i] = app.cards[random];
+                app.cards[random] = temp;
+            }
         },
     }
     app.init();
