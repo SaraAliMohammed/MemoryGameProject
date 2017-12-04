d4163451ef6f7117fb75cdf3f96d4d902cea860f
sara_ali <sara.ali.mohammed213@gmail.com>
2017-12-04 11:25:28 +0200

    Create a list that holds all of cards and init function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
-
+    let app = {
+        cards: ['fa-diamond', 'fa-diamond', 'fa-paper-plane-o', 'fa-paper-plane-o',
+        'fa-anchor', 'fa-anchor', 'fa-bolt', 'fa-bolt', 'fa-cube', 'fa-cube', 'fa-leaf',
+        'fa-leaf', 'fa-bicycle', 'fa-bicycle', 'fa-bomb', 'fa-bomb'],
+        moves: 0,
+        rating: 3,
+        startTime: 0,
+        init: function () {
+            app.startTime = new Date().getTime();
+            app.moves = 0;
+            $('.moves').text('0');
+            if($('.fa-star-o').length !== 0)
+                $('.stars i').removeClass('fa-star-o').addClass('fa-star');
+            $('.deck').html("");
+        },
+    }
+    app.init();
 });
